4583345ec4ce8998276d8be3374d19632521ca1f
Eric Onofrey <38504767+eonofrey@users.noreply.github.com>
2019-03-22 16:36:41 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup 
 import pandas as pd 
 
+# Function to grab the store address from each page
 def grab_address(url = 'https://www.containerstore.com/locations/showStore.htm?store=GAL'):
     try:
         a = requests.get(url)
@@ -19,7 +20,7 @@ url = 'https://www.containerstore.com/locations/index.htm'
 page_response = requests.get(url)
 page_content = BeautifulSoup(page_response.text, 'html.parser')
 
-# Grab all stores
+# Grab all stores from the main page 
 stores = page_content.findAll(class_="stateListItem storeName")
 
 # Grab all store url's 
@@ -35,5 +36,6 @@ address_container = []
 for i in store_urls:
     address_container.append(grab_address(i))
     
+# Output a .csv file 
 pd.DataFrame(address_container, columns = ["Address"])
 
