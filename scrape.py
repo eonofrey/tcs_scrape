2aea26d35ce57b74e8c25b970110eda660a665d9
Eric Onofrey <38504767+eonofrey@users.noreply.github.com>
2019-03-22 16:35:14 -0400

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,39 @@
+# Imports
+import requests
+from bs4 import BeautifulSoup 
+import pandas as pd 
+
+def grab_address(url = 'https://www.containerstore.com/locations/showStore.htm?store=GAL'):
+    try:
+        a = requests.get(url)
+        b = BeautifulSoup(a.text, 'html.parser')
+        address = b.find('address').text.strip().replace("                  ", ' ').replace(u'\xa0', ' ')
+        address = address.replace("\n","")
+        print(address)
+        return address
+    except HTTPError as e: 
+        return None
+
+# Set up the Soup
+url = 'https://www.containerstore.com/locations/index.htm'
+page_response = requests.get(url)
+page_content = BeautifulSoup(page_response.text, 'html.parser')
+
+# Grab all stores
+stores = page_content.findAll(class_="stateListItem storeName")
+
+# Grab all store url's 
+store_urls= []
+
+for store in stores: 
+    surl = (store.find('a')['href'])
+    store_urls.append('https://www.containerstore.com' + surl)
+
+# Get addresses
+address_container = []    
+
+for i in store_urls:
+    address_container.append(grab_address(i))
+    
+pd.DataFrame(address_container, columns = ["Address"])
+
